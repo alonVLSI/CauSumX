4b9de670a91d57873b604285eb6e9c0e457ca7f5
Alon Hertzog <Alon.Hertzog@Hexagon.com>
2024-08-12 09:13:01 +0300

    1) delete redundant file 2) add num of males, females and summary

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ from itertools import combinations
 APRIORI = 0.6
 COLUMNS_TO_EXCLUDE = ['education.num', 'capital.gain', 'capital.loss', 'income','fnlwgt']
 COLUMNS_TO_EXCLUDE_WITHOUT_SALARY = ['education.num', 'capital.gain', 'capital.loss', 'income']
-NUM_OF_FINAL_GROUPS = 7
-KEYS_TO_REMOVE=['female_treatment_effect','treatment_effect','score']
+NUM_OF_FINAL_GROUPS = 3
 TARGET_SALARY = 'income'
 DAG = [
     'age;',
@@ -85,13 +84,11 @@ def process_group(df_origin, df, group, protected_column, protected_column_value
     remaining_attrs = [attr for attr in df.columns if attr not in excluded_attrs]
     
     treatments = Utils.getLevel1treatments(remaining_attrs, df)
-
     group_df = df_origin.copy()
     for k, v in group.items():
         group_df = group_df[group_df[k] == v]
     group_size = len(group_df)
-    num_females = len(group_df[group_df[protected_column] == protected_column_value])
-    num_males = group_size - num_females
+
     dict_for_best_treatments =  dict()
     general_and_female_effectt=[]
     for treatment in treatments:
@@ -119,6 +116,8 @@ def process_group(df_origin, df, group, protected_column, protected_column_value
     result = {
             'group': group,
             'group_size': group_size,
+            'males':0,
+            'females':0,
             'treatment': dict_for_best_treatments[json.dumps(group)][0],
             'treatment_effect': general_and_female_effectt[0],
             'female_treatment_effect': general_and_female_effectt[1],
@@ -127,38 +126,47 @@ def process_group(df_origin, df, group, protected_column, protected_column_value
         
     results.append(result)
 
-def so(protected_column, protected_column_value):
+def CalculateEqualyTreatments(protected_column, protected_column_value):
     start_time = time.time()
     df = pd.read_csv(PATH + 'adult_new.csv', encoding='utf8')
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     df = df.dropna()
     df_origin = df.copy()
     df = df.drop(columns=COLUMNS_TO_EXCLUDE)
+    total_size = len(df_origin)
     groups = Algorithms.getAllGroups(df, APRIORI)
     results = []
 
     for group in groups:
         process_group(df_origin, df, group, protected_column, protected_column_value, results)
 
-    results = greedy_selection(df_origin, NUM_OF_FINAL_GROUPS, results)
+    results = greedy_selection(df_origin, NUM_OF_FINAL_GROUPS, results,protected_column, protected_column_value)
     results.sort(key=lambda x: str(x['group']))
+
     expectation_grade = calculateExpectation(results,df_origin)
     protected_expectation_grade = calculateExpectation(results,df_origin,{protected_column:protected_column_value})
     end_time = time.time()
     overall_time = end_time-start_time
-    results[0]['expectation_grade'] = expectation_grade
-    results[0]['protected_expectation_grade'] = protected_expectation_grade
-    results[0]['Total time(seconds)'] = overall_time
-    keys = results[0].keys()
-    filtered_keys = [key for key in keys if key not in KEYS_TO_REMOVE]
-    filtered_results = []
-    for result in results:
-        filtered_result = {key: value for key, value in result.items() if key not in KEYS_TO_REMOVE}
-        filtered_results.append(filtered_result)    
-    with open(f'treatment_effects_greedy_{NUM_OF_FINAL_GROUPS}_final_groups_{APRIORI}AP.csv', 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, fieldnames=filtered_keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(filtered_results)
+    total_coverage = sum(item['group_size'] for item in results)
+    summary = {
+        'expectation_grade': expectation_grade,
+        'protected_expectation_grade': protected_expectation_grade,
+        'Total time(seconds)': overall_time,
+        'Total amount': total_size,
+        'Total Coverage': total_coverage,
+        'Coverage Percentae': f"{(total_size/total_coverage):.2f}%"
+    }
+
+    with open(f'treatment_effects_greedy_{NUM_OF_FINAL_GROUPS}_final_groups_{APRIORI}AP.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        if results:
+            writer.writerow(results[0].keys())
+        for item in results:
+            writer.writerow(item.values())
+        writer.writerow([])
+        if summary:
+            writer.writerow(summary.keys())  
+            writer.writerow(summary.values())
 
 def addTempTreatment(row, treatment, ordinal_atts):
     for att, val in treatment.items():
@@ -178,32 +186,23 @@ def getAttsVals(atts, df):
         atts_vals[att] = df[att].unique().tolist()
     return atts_vals
 
-def greedy_selection(df, k, results):
-        #result = {
-        #    'group': group,
-        #    'group_size': group_size,
-        #    'num_females': num_females,
-        #    'num_males': num_males,
-        #    'treatment': dict_for_best_treatments[json.dumps(group)][0],
-        #    'treatment_effect': general_and_female_effectt[0],
-        #    'female_treatment_effect': general_and_female_effectt[1],
-        #    'utility_grade': dict_for_best_treatments[json.dumps(group)][1]
-        #}
+def greedy_selection(df, k, results,protected_column,protected_column_value):
     selected_result = []
-
     for _ in range(k):
         best_group = None
         best_result = None
         best_group_size = 0
         best_score = -1
-
+        num_females = 0
+        num_males = 0
         # Find the group with the highest score (group_size * utility_grade)
         for result in results:
             group = result['group']
             utility_grade = result['utility_grade']
             
             # Calculate the current size of the group in the DataFrame
-            group_size = df[df.apply(lambda row: all(row[k] == v for k, v in group.items()), axis=1)].shape[0]
+            filtered_df = df[df.apply(lambda row: all(row[k] == v for k, v in group.items()), axis=1)]
+            group_size = len(filtered_df)
             
             score = group_size * utility_grade
             if score ==0:
@@ -214,13 +213,15 @@ def greedy_selection(df, k, results):
                 best_group = group
                 best_result = result
                 best_group_size = group_size
-
+                num_females = len(filtered_df[filtered_df[protected_column] == protected_column_value])
+                num_males = group_size - num_females
         if best_group is None:
             break
         
-        best_result['score'] = best_score
         best_result['group'] = best_group
         best_result['group_size'] = best_group_size
+        best_result['males'] = num_males
+        best_result['females'] = num_females
         selected_result.append(best_result)
 
         group_df = df[df.apply(lambda row: all(row[k] == v for k, v in best_group.items()), axis=1)]
@@ -272,7 +273,7 @@ def calculateExpectation(results, df,protected=None):
 def main():
    protected_column='sex'
    protected_value = 'Female'
-   so(protected_column, protected_value)
+   CalculateEqualyTreatments(protected_column, protected_value)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
